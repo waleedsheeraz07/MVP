7090d33a4a96a674c3552fa2b2e7fe6d2c5e96fe
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-08-31 09:04:21 +0300

    Create next-auth.d.ts

---
+++ my-app/types/next-auth.d.ts
@@ -0,0 +1,24 @@
+// types/next-auth.d.ts
+import NextAuth from "next-auth"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string
+      email: string
+      role: string
+    }
+  }
+
+  interface User {
+    id: string
+    role: string
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string
+    role: string
+  }
+}
