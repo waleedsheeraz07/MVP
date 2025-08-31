20ec2126516c34352f6a7c4b94c70de69ad62454
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-08-31 05:58:08 +0000

    modified:   my-app/package-lock.json modified:   my-app/package.json 	modified:   my-app/pages/api/auth/[...nextauth].ts

--- my-app/pages/api/auth/[...nextauth].ts
+++ my-app/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { PrismaClient } from "@prisma/client"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 
 const prisma = new PrismaClient()
 
