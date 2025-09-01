0a52766993c1b78b041d5cc1513f1f8303eacb2b
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-01 17:03:04 +0300

    Update cloudinary.ts

--- my-app/lib/cloudinary.ts
+++ my-app/lib/cloudinary.ts
@@ -2,7 +2,7 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  cloud_name: process.env.CLOUD_NAME!,
   api_key: process.env.API_KEY!,
   api_secret: process.env.API_SECRET!,
 });
