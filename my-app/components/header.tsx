0fe81d9c174e371cd4dfe979052201b1e629e962
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-02 09:45:38 +0300

    Update header.tsx

--- my-app/components/header.tsx
+++ my-app/components/header.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 import Link from 'next/link'
-import styles from '../../styles/header.module.css'
+import styles from '../styles/header.module.css'
 
 type AdminHeaderProps = {
   title?: string
