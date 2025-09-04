368583304a46e78a00627cb5439caf676f824236
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-04 23:46:14 +0300

    Update profile.tsx

--- my-app/pages/profile.tsx
+++ my-app/pages/profile.tsx
@@ -1,7 +1,7 @@
 import { GetServerSidePropsContext } from "next";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "./api/auth/[...nextauth]";
-import prisma from "../lib/prisma";
+import { prisma } from "../lib/prisma";
 
 interface ProfilePageProps {
   user: {
