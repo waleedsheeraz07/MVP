f194576c63bc78543719b616ef33d6878a738c64
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-09-04 10:47:00 +0300

    Update _app.tsx

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,6 +1,14 @@
+// pages/_app.tsx
+"use client"; // Ensures we can use client hooks like useState/useEffect
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { CartProvider } from "../context/CartContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
+  );
 }
