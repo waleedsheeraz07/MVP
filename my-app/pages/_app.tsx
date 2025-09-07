c8c343f4fe5c44523eaa3711e9005ef7f82be5d8
waleedsheeraz07 <waleedsheeraz07@gmail.com>
2025-09-07 12:39:45 +0300

    Update _app.tsx

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,6 +1,5 @@
 // pages/_app.tsx
 "use client"; // Ensures we can use client hooks like useState/useEffect
-import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
