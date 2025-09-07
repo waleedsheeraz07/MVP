272a64eba0181a2d6d3f54e70fbe6b78a68d067f
waleedsheeraz07 <waleedsheeraz07@gmail.com>
2025-09-07 11:37:52 +0300

    Update _app.tsx

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,14 +1,23 @@
 // pages/_app.tsx
 "use client"; // Ensures we can use client hooks like useState/useEffect
-
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { CartProvider } from "../context/CartContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+<>
+<Head>
+        <link rel="icon" type="image/png" href="/favicon.PNG" />
+<link rel="apple-touch-icon" href="/favicon.PNG" />
+  
+     </Head>
     <CartProvider>
       <Component {...pageProps} />
     </CartProvider>
+
+</>
   );
 }
