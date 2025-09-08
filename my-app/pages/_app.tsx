f2e7f1a75cca255b39e08a9432f783521ee23a5c
waleedsheeraz07 <waleedsheeraz07@gmail.com>
2025-09-08 08:50:54 +0300

    Update _app.tsx

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -4,13 +4,10 @@ import Head from 'next/head'
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { CartProvider } from "../context/CartContext";
-import ForceLogout from "../components/ForceLogout";
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
 <>
- <ForceLogout />
 <Head>
         <link rel="icon" type="image/png" href="/favicon.PNG" />
 <link rel="apple-touch-icon" href="/favicon.PNG" />
