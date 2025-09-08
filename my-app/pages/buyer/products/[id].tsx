70ff513a63191af640767cbe1526a171db868bda
waleedsheeraz07 <waleedsheeraz07@gmail.com>
2025-09-08 20:43:52 +0300

    Update [id].tsx

--- my-app/pages/buyer/products/[id].tsx
+++ my-app/pages/buyer/products/[id].tsx
@@ -340,7 +340,7 @@ const carouselHandlers = useSwipeable({
 </div>
 {/* Full-screen mobile gallery */}
 {galleryOpen && (
-  <div className="fixed inset-0 bg-black bg-opacity-90 z-50 flex items-center justify-center">
+  <div className="fixed inset-0 bg-transparent backdrop-blur-md z-50 flex items-center justify-center">
     <button
       className="absolute top-4 right-4 text-white text-2xl z-50"
       onClick={() => setGalleryOpen(false)}
