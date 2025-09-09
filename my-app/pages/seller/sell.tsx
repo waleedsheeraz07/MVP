9ea553e5264c9fb3b89325dddd91fc5d495bcc3b
waleedsheeraz07 <waleedsheeraz07@gmail.com>
2025-09-09 19:14:28 +0300

    Update sell.tsx

--- my-app/pages/seller/sell.tsx
+++ my-app/pages/seller/sell.tsx
@@ -196,10 +196,12 @@ const [selectedCategories, setSelectedCategories] = useState<string[]>([])
     setLoading(true)
     setError("")
 
-if (sizes.length === 0) {
-    setError("Please select at least one size.");
-    setLoading(false);
-    return;
+if (sizes.length === 0 || !condition) {
+  if (sizes.length === 0) setError("Please select at least one size.");
+  else if (!condition) setError("Please select a condition.");
+
+  setLoading(false);
+  return;
 }
     try {
       const formData = new FormData()
